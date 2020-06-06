ff0a47e81549bed0e348f9fd186ebe45bf5f9f72
Mike <cyplesma@aol.com>
2020-06-05 23:44:35 -0400

    Add files via upload

---
+++ ThisPc/ThisPc/frmMain.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ThisPc
+{
+    partial class frmName
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmName));
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblKey = new System.Windows.Forms.Label();
+            this.tmrKey = new System.Windows.Forms.Timer(this.components);
+            this.lblIP = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.Lime;
+            this.lblName.Location = new System.Drawing.Point(25, 10);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(61, 16);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "PC Name";
+            // 
+            // lblKey
+            // 
+            this.lblKey.AutoSize = true;
+            this.lblKey.BackColor = System.Drawing.Color.Transparent;
+            this.lblKey.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKey.ForeColor = System.Drawing.Color.Lime;
+            this.lblKey.Location = new System.Drawing.Point(404, 10);
+            this.lblKey.Name = "lblKey";
+            this.lblKey.Size = new System.Drawing.Size(228, 16);
+            this.lblKey.TabIndex = 2;
+            this.lblKey.Text = "KEY: ________________________________";
+            // 
+            // tmrKey
+            // 
+            this.tmrKey.Interval = 30000;
+            this.tmrKey.Tick += new System.EventHandler(this.tmrKey_Tick);
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.BackColor = System.Drawing.Color.Transparent;
+            this.lblIP.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIP.ForeColor = System.Drawing.Color.Lime;
+            this.lblIP.Location = new System.Drawing.Point(196, 10);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(72, 16);
+            this.lblIP.TabIndex = 1;
+            this.lblIP.Text = "IP Address";
+            this.lblIP.Click += new System.EventHandler(this.lblIP_Click);
+            // 
+            // frmName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(657, 34);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblKey);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.lblName);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmName";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "This PC: ";
+            this.TransparencyKey = System.Drawing.Color.Red;
+            this.Load += new System.EventHandler(this.frmName_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblKey;
+        private System.Windows.Forms.Timer tmrKey;
+        private System.Windows.Forms.Label lblIP;
+    }
+}
+
---
+++ ThisPc/ThisPc/frmMain.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThisPc
+{
+    public partial class frmName : Form
+    {
+
+        IPAddress[] arsIPAddresses;
+        int iIPAddressCount = 0;
+        int iCurrentIPAddress = 0;
+        string gsIPAddress = "OOO";
+        public frmName()
+        {
+            InitializeComponent();
+        }
+
+        private void frmName_Load(object sender, EventArgs e)
+        {
+            this.Left = 300;
+            this.Top = 0;
+            lblName.Text = Environment.MachineName.ToUpper();
+
+            Bitmap bmp = new Bitmap(this.BackgroundImage);
+            bmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+            
+            bmp.MakeTransparent(bmp.GetPixel(2, 2));            
+            this.BackgroundImage = null;
+            this.BackgroundImage = bmp;
+
+
+            CollectIPAddresses();
+            GetHost();
+            lblIP.Text = gsIPAddress;
+            SetKeyValue();
+
+
+
+            tmrKey_Tick(this, null);
+
+        }
+
+        private void CollectIPAddresses()
+        {
+            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable() == true)
+            {
+                IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+                arsIPAddresses = host.AddressList;
+                iIPAddressCount = host.AddressList.Length;
+            }
+            else
+            {
+                lblIP.Text = "Not Connected!!";
+            }
+
+        }
+
+        private void lblIP_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tmrKey_Tick(object sender, EventArgs e)
+        {
+
+            tmrKey.Enabled = false;
+
+            SetKeyValue();
+
+            //lblIP.Text = arsIPAddresses[iCurrentIPAddress].ToString();
+            iCurrentIPAddress++;
+            if (iCurrentIPAddress >= iIPAddressCount)
+            {
+                CollectIPAddresses();
+                iCurrentIPAddress = 0;
+            }
+
+
+            tmrKey.Enabled = true;
+        }
+
+
+        private void SetKeyValue()
+        {
+
+            Random rSeed = new Random();
+            string sKey = "";
+            for (int i = 0; i < 32; i++)
+            {
+                int rChar = rSeed.Next(33, 126);
+
+                sKey += Char.ConvertFromUtf32(rChar);
+            }
+
+            lblKey.Text = sKey;
+        }
+
+        public void GetHost()
+        {
+            string gsHostName = Dns.GetHostName();
+
+            
+
+            for(Int16 i = 0; i < System.Net.Dns.GetHostEntry(gsHostName).AddressList.Length ; i++)
+            { 
+            if( System.Net.Dns.GetHostEntry(gsHostName).AddressList[i].IsIPv6LinkLocal == false )
+                    {
+                     gsIPAddress = System.Net.Dns.GetHostEntry(gsHostName).AddressList[i].ToString();
+                if( gsIPAddress.Contains(".") == true)
+                    {
+                        return;
+                }
+
+            }
+
+        }
+        //gsIPAddress = System.Net.Dns.GetHostEntry(gsHostName).AddressList(0).ToString() 
+
+
+    }
+    }
+}
